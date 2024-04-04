8f3fe7b6dd17f5a2c7b2fdd7ba89f7e3dcff5cbd
oggosil <airtown.yang@gmail.com>
2024-04-04 21:24:03 +0900

    se.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface postModel {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): postModel {
-    return {
-      author: 'string',
-      title: 'string',
-      content: 'string',
-      likeCount: 0,
-      commentCount: 0,
-    };
+  getHello() {
+    return 'hello world';
   }
 }
